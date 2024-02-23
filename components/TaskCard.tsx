888ab7e6608a29374bebe1044a2032e3b2994714
Yuriy Pilipenko <yurik2061@gmail.com>
2024-02-23 23:58:12 +0200

    added checkboxes ot taskcard

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -4,28 +4,41 @@ import { Task } from "@prisma/client"
 import { Checkbox } from "./ui/checkbox"
 import { format } from "date-fns"
 import { cn } from "@/lib/utils"
+import { useTransition } from "react"
+import { setTaskToDone } from "@/actions/task"
+import { useRouter } from "next/navigation"
+
+const getExpirationColor =(expiresAt: Date) => {
+    const days = Math.floor(expiresAt.getTime() - Date.now()) / 1000 / 60 / 60;
+    if (days < 0) {
+        return "text-grey-500 dark:text-grey-400"
+    }
+    if (days <= 3 * 24) {
+        return "text-red-500 dark:text-red-400"
+    }
+    if (days <= 7 * 24) {
+        return "text-orange-500 dark:text-orange-400"
+    }
+        return "text-green-500 dark:text-green-400"
+}
 
 function TaskCard({task}: {task: Task}) {
+    const [isLoading, startTransition] = useTransition()
+    const router = useRouter()
 
-    const getExpirationColor =(expiresAt: Date) => {
-        const days = Math.floor(expiresAt.getTime() - Date.now()) / 1000 / 60 / 60;
-        if (days < 0) {
-            return "text-grey-500 dark:text-grey-400"
-        }
-        if (days <= 3 * 24) {
-            return "text-red-500 dark:text-red-400"
-        }
-        if (days <= 7 * 24) {
-            return "text-orange-500 dark:text-orange-400"
-        }
-            return "text-green-500 dark:text-green-400"
-    }
   return (
     <div className="flex gap-2 items-start">
         <Checkbox 
             className="w-5 h-5 "
             checked={task.done}
+            disabled={task.done || isLoading}
             id={task.id.toString()}
+            onCheckedChange={() => {
+                startTransition(async () => {
+                    await setTaskToDone(task.id)
+                    router.refresh()
+                })
+            }}
         />
         <label 
             htmlFor={task.id.toString()}
